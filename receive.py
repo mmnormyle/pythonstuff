da7ff7df10218b394c60b0f03de06f96d062befd
Matthew Normyle <mmnormyle@gmail.com>
2015-11-03 23:50:03 -0600

    asoifjas

--- receive.py
+++ receive.py
@@ -40,6 +40,16 @@ while len(magnitudedata)<NUM_DATA:
 
 print 'done!'
 
+
+out = np.fft.rfft(data,128)
+
+print np.angle(out, deg=True)
+
+powers = []
+for x in range(0, len(out)):
+	powers.append(abs(out[x]))
+
+
 power_mean = np.mean(magnitudedata)
 power_std = np.std(magnitudedata)
 magXPoints = []
@@ -50,7 +60,7 @@ for x in range(0,NUM_DATA):
 	zscore = (magnitudedata[x]-power_mean)/power_std
 	if(zscore>2 and x<=(NUM_DATA/2)):
 		sigfrequencies.append(x)
-	magYPoints.append(zscore)
+	magYPoints.append(magnitudedata[x])
 
 SAMPLING_FREQ = 915000
 
@@ -66,11 +76,14 @@ for x in range(0,NUM_DATA):
 	xPoints.append(x)
 	yPoints.append(data[x])
 
+plt.figure(1)
+plt.subplot(211)
 plt.scatter(xPoints, yPoints)
-plt.show()
 
+plt.subplot(212)
 plt.scatter(magXPoints, magYPoints)
 plt.show()
+
 
 
 
--- sinusoidlookuptable.py
+++ sinusoidlookuptable.py
@@ -4,19 +4,15 @@ cos_lookuptable = []
 sin_lookuptable = []
 
 data_size = 128
-scale = 8
+scale = 6
 
 for n in range(0, data_size):
 	cos_lookuptable.append(int(
-		np.cos(2*np.pi*float(n)/float(data_size)) * pow(2,scale)))
+		np.cos(2*np.pi*float(n)/float(data_size)) * 1000))
 	sin_lookuptable.append(int(
-		np.sin(2*np.pi*float(n)/float(data_size)) * pow(2,scale)))
+		np.sin(2*np.pi*float(n)/float(data_size)) * 1000))
 
 	
-#for x in range (0, len(cos_lookuptable)):
-#	print(str(float(cos_lookuptable[x])/float(pow(2,scale)) - np.cos(2*np.pi*float(x)/float(data_size)) ) + ','),
-#	if((x+1)%8==0):
-#		print
 print "cos: "
 for x in range (0, len(cos_lookuptable)):
 	print(str(cos_lookuptable[x]) + ','),
